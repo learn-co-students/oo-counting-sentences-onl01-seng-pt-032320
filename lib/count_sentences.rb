d8182528c6a6c693c85efc2a0f6301e92de8f3ad
Dina Kendle <dina.kendle@gmail.com>
2020-04-11 00:09:27 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter = 0
+    split_words = []
+    split_words << self.split(".","?","!")
+      counter += 1
   end
+  split_words
 end
