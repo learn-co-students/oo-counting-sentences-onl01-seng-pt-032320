c428fee4d0be67ad5c0f8eecdc6de6369d0d3eed
Malcom Fetus <sora.vista@gmail.com>
2020-04-15 22:35:19 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -33,5 +33,9 @@ self.end_with?("!")
     #binding.pry
 
 self.split(/\.|!|\?/).reject {|e| e.empty?}.length
+#^This line of code here is splitting the self keyword into an array bu identifying the specific RegEx to help divide the sentences
+#then this causes 5 sentences and counts an empty string ("") as a sentence
+#then we use the .reject method to remove any empty strings ("")
+#then we used the.length to count the entire self keyword for the count_sentences method.
   end
 end
